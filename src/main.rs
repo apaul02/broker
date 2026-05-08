2b298e82684738f65419d9da0497555e4a3b32d9
apaul02 <captainankan@gmail.com>
2026-05-09 00:07:02 +0530

    uhhh

--- src/main.rs
+++ src/main.rs
@@ -1,4 +1,34 @@
+use std::collections::HashMap;
+
+use bytes::Bytes;
+use tokio::sync::mpsc;
+
+use crate::models::{BrokerState, Command};
+
 pub mod models;
-fn main() {
-    println!("Hello, world!");
+#[tokio::main]
+async fn main() {
+    let (tx, mut rx) = mpsc::channel::<Command>(1024);
+    let state = BrokerState {
+        topics: HashMap::new(),
+    };
+    tokio::spawn(async move {
+        state.listen(rx).await;
+    });
+    let socket = tokio::net::TcpListener::bind("127.0.0.1:8080")
+        .await
+        .unwrap();
+    loop {
+        let a = socket.accept().await.unwrap();
+        let t = tx.clone();
+        tokio::spawn(async move {
+            let (resp_tx, resp_rx) = tokio::sync::oneshot::channel();
+            let cmd = Command::Publish {
+                topic_name: "Idk".to_string(),
+                payload: Bytes::from("yooo"),
+                responder: resp_tx,
+            };
+            t.send(cmd).await.unwrap();
+        });
+    }
 }
